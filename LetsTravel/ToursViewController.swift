0223d88411de88f8f33a7c20de619a2529920e64
TriNgo <ngominhtrint@gmail.com>
2016-04-19 22:28:56 +0700

    - add UIPageViewController

--- LetsTravel/ToursViewController.swift
+++ LetsTravel/ToursViewController.swift
@@ -8,19 +8,69 @@
 
 import UIKit
 
-class ToursViewController: UIViewController {
-
+class ToursViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    private var pageViewController: UIPageViewController?
+    
+    private let contentImages = ["nature_pic_1.png",
+        "nature_pic_2.png",
+        "nature_pic_3.png",
+        "nature_pic_4.png"];
+    
+    private let hotTours: [Tour]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createPageViewController()
+        setupPageControl()
         // Do any additional setup after loading the view.
     }
 
+    private func createPageViewController() {
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let navigationBarHeight = self.navigationController?.navigationBar.frame.height
+        let pageController = self.storyboard?.instantiateViewControllerWithIdentifier("PageController") as! UIPageViewController
+        pageController.dataSource = self
+        pageController.view.frame = CGRectMake(0, statusBarHeight + navigationBarHeight!, self.view.frame.width, 300)
+
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers: NSArray = [firstController]
+            pageController.setViewControllers(startingViewControllers as? [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
+        
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.greenColor()
+        appearance.backgroundColor = UIColor.whiteColor()
+    }
+    
+    private func loadData() {
+        TravelClient.sharedInstance.getHotTours({ (response:[Tour]) -> () in
+            <#code#>
+            }) { (<#NSError#>) -> () in
+                <#code#>
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onLogoutClick(sender: UIButton) {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        TravelClient.sharedInstance.logout()
+    }
 
     /*
     // MARK: - Navigation
@@ -32,4 +82,52 @@ class ToursViewController: UIViewController {
     }
     */
 
+    // MARK: - UIPageViewControllerDataSource
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex - 1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex + 1 < contentImages.count {
+            return getItemController(itemController.itemIndex + 1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
 }
+
+
+
+
+
