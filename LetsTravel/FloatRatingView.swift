31b87c3e81dfb74db2b587a55e92aa55fefdbf3e
TriNgo <ngominhtrint@gmail.com>
2016-04-27 13:46:59 +0700

    add rating view

---
+++ LetsTravel/FloatRatingView.swift
@@ -0,0 +1,21 @@
+//
+//  FloatRatingView.swift
+//  LetsTravel
+//
+//  Created by TriNgo on 4/27/16.
+//  Copyright © 2016 TriNgo. All rights reserved.
+//
+
+import UIKit
+
+class FloatRatingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
